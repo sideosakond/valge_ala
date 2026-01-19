c4e6b11ea93d78b6df50bf3482433d285b13dcef
sideosakond <tehnilisejarelevalveamet@gmail.com>
2026-01-19 10:14:35 +0200

    Add files via upload

--- data/main.js
+++ data/main.js
@@ -38,7 +38,7 @@ function getRandomHexColor() {
 
 let geojson = geojson_array[0];
 let features = new ol.format.GeoJSON().readFeatures(geojson, {dataProjection: 'EPSG:3301', featureProjection: 'EPSG:3301'});
-let hex_color = '#ff3a5a';
+let hex_color = getRandomHexColor();
 
 const layer = new ol.layer.Vector({
   //declutter: true,
